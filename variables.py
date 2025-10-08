46de168c79975fd391f91f3991c4fecf039fdb2d
Jurgsl <60768303+Jurgsl@users.noreply.github.com>
2025-10-08 14:24:09 +1300

    My first Python files that I created in class

---
+++ variables.py
@@ -0,0 +1,10 @@
+# Demonstrate how variable are created and how they work.
+# Author: Juergen Lier
+# Date: 19-09-2025
+# Version 1.0
+
+# Different types of variables
+# Store a string
+greeting = "Hello World!"
+print(greeting)
+
