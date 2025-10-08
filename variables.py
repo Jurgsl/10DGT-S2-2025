ce677d81d7dde4870cb8dbb4e3ec2d96bfeb48f3
Jurgsl <60768303+Jurgsl@users.noreply.github.com>
2025-10-08 14:59:33 +1300

    Variables version 1.1.
    I added a few more ways of creating variables

--- variables.py
+++ variables.py
@@ -1,10 +1,52 @@
 # Demonstrate how variable are created and how they work.
 # Author: Juergen Lier
-# Date: 19-09-2025
-# Version 1.0
+# Date: 08-10-2025
+# Version 1.1
 
 # Different types of variables
 # Store a string
 greeting = "Hello World!"
 print(greeting)
+
+# Store a number
+my_number = 80 # assigned 80 to the variable called my_number
+print(my_number)
+
+my_number2 = 7
+print(my_number2)
+
+# Assign the value of one variable to another
+my_number = greeting
+print(my_number) # Don't assign values to variables that don't make sense for its name
+
+'''Do calculations with variables and store the result
+in another variable. I can now
+press enter
+as many times
+as 
+I 
+like'''
+
+# Creating new variable
+num_1 = 5
+num_2 = 18
+
+sum1 = 5 + 18 # This is not good practice.
+print(sum1)
+
+sum1 = num_1 + num_2
+print(sum1)
+
+sum_string1 = "18"
+sum_string2 = "5"
+
+# Adding two strings together. This is called concatenation
+sum = sum_string1 + sum_string2
+print(sum)
+
+# Print formatting. f stands for 'format' and insert the value of variables into the curly brackets
+print(f"My calculation for adding {num_1} and {num_2} together is {sum1}.")
+print("My calculation for adding {} and {} together is {}.".format(num_1, num_2, sum1)) # old way of formatting
+
+
 
