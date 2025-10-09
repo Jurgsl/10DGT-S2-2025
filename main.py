adb81100dac000c6a8aec1d1232cb750db58c3c1
Jurgsl <60768303+Jurgsl@users.noreply.github.com>
2025-10-10 10:55:41 +1300

    Version 2
    Added a while loop

--- main.py
+++ main.py
@@ -2,17 +2,33 @@
 # Juergen Lier
 # Date: 10/10/2025
 
-# Version 1
+# Version 2
 # TODO: Ask the user if they like coffee
 #       Record the answer
 #       Give a response back to the answer
 
+# Version 1
 # Ask the user whether they like coffee or not
-like_coffee = input("Do you like coffee? ")
+'''like_coffee = input("Do you like coffee? ")
 print(f'Your answer was "{like_coffee}".')
 
 if like_coffee == "yes" or "yes" or "y" or "Y":
     print("That is great! I like coffee too.")
 
 else:
-    print("You are missing out! Why not give it a try?")
+    print("You are missing out! Why not give it a try?")'''
+
+# Version 2
+# While loop
+keep_going = ""
+while keep_going == "":
+    like_coffee = input("Do you like coffee? ")
+
+    if like_coffee == "yes" or like_coffee == "Yes" or like_coffee == "y" or like_coffee == "Y":
+        print("That is great! I like coffee too.")
+    
+    elif like_coffee == "no" or like_coffee == "No" or like_coffee == "n":
+        print("You are missing out! Why not give it a try?")
+
+    else:
+         print("I don't understand. ")
