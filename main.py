aa1530aef8879bc27406e4f6bfcb50375dfd337f
Jurgsl <60768303+Jurgsl@users.noreply.github.com>
2025-10-24 10:06:14 +1300

    Version 1.3 Added coffee program into a function

--- main.py
+++ main.py
@@ -35,31 +35,36 @@ while keep_going == "":
          print("I don't understand. ")'''
 
 
-# Version 3
+# Version 3.1
 # Making the program more pythonic.
-keep_going = ""
-while keep_going == "":
-    # Convert answer to lower case using .lower()
-    like_coffee = input("Do you like coffee? ").lower()
-    if like_coffee == "yes" or like_coffee == "y":
-        print("That's great! I like coffee too!")
-    
-    elif like_coffee == "no" or like_coffee == "n":
-        print("You are missing out! Why not give it a try?")
+def coffee_program():
+    keep_going = ""
+    while keep_going == "":
+        # Convert answer to lower case using .lower()
+        like_coffee = input("Do you like coffee? ").lower()
+        if like_coffee == "yes" or like_coffee == "y":
+            print("That's great! I like coffee too!")
+        
+        elif like_coffee == "no" or like_coffee == "n":
+            print("You are missing out! Why not give it a try?")
 
-        like_tea = input("Do you like tea instead?").upper() # Convert input to upper case using .upper()
-        if like_tea == "YES" or like_tea == "Y":
-            print("Good for you. Give coffee another try :)")
+            like_tea = input("Do you like tea instead? ").upper() # Convert input to upper case using .upper()
+            if like_tea == "YES" or like_tea == "Y":
+                print("Good for you. Give coffee another try :)")
 
-        elif like_tea == "NO" or like_tea == "N":
-            print("I am sorry. That is all I have for now.")
+            elif like_tea == "NO" or like_tea == "N":
+                print("I am sorry. That is all I have for now.")
 
-        else:
+            else:
+                print("I don't understand. Please answer with either Yes or No.")
+
+        else: # Error message
             print("I don't understand. Please answer with either Yes or No.")
 
-    else: # Error message
-        print("I don't understand. Please answer with either Yes or No.")
+        keep_going = input("Press <ENTER> to continue, or any other key to quit. Thanks!")
 
-    keep_going = input("Press <ENTER> to continue, or any other key to quit. Thanks!")
+if __name__ == "__main__":
+    coffee_program()
+    
 
     
